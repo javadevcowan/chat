2b22f841922d1fea6a228f14dabfb1d141f5d16a
Anthony Cowan <javadevcowan@gmail.com>
2018-10-14 12:15:22 +0100

    first commit

---
+++ backend/src/main/java/de/jonashackt/springbootvuejs/SpringBootVuejsApplication.java
@@ -0,0 +1,13 @@
+package de.jonashackt.springbootvuejs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+
+@SpringBootApplication
+public class SpringBootVuejsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootVuejsApplication.class, args);
+	}
+}
