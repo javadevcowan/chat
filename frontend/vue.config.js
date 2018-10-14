834fac548266713f4e245923ade980e8a84f965f
Anthony Work <anthonyc@anthonys-pro.home>
2018-10-14 13:33:52 +0100

    Fixed build

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -14,6 +14,5 @@ module.exports = {
   },
   // Change build paths to make them Maven compatible
   // see https://cli.vuejs.org/config/
-  outputDir: 'target/dist',
-  assetsDir: 'static'
+  outputDir: 'build'
 }
